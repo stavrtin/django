aa79df999a6b9f87af39e84d773347821b359a21
stavrtin <stavrtin@yandex.ru>
2023-10-08 19:24:04 +0300

    дз2 миграции

--- sem02_task1_app/www.py
+++ sem02_task1_app/www.py
@@ -1,3 +1,10 @@
-dict_result = {'орел': 0, 'решка': 0}
+import random
 
-print(dict_result)
+# declaring list
+list = [2, 2, 4, 6, 6, 8]
+
+# initializing the value of n
+n = 4
+
+# printing n elements from list
+print(random.sample(list, n))
