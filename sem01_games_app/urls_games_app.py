635e817798b5578171e5c31ec64e21044e531d8c
stavrtin <stavrtin@yandex.ru>
2023-10-11 23:39:32 +0300

    семинар_3

--- sem01_games_app/urls_games_app.py
+++ sem01_games_app/urls_games_app.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path("game1", views.game1, name="game1"),
     path("game2", views.game2, name="game1"),
-    path("game3", views.game3, name="game1"),
+    path("game3/<int:count_drop>", views.game3, name="game1"),
     path("select_all_n <int:n>", views.select_all_n, name="select_all_n"),
-    # path("about/", views.about, name="about"),
-    # path("new/", views.new, name="new"),
-]
+    path("start/<str:name>/", views.view_start, name="view_start"),
+    path("author/<int:author_id>", views.view_author_art, name="view_author_art"),
+    path("articl_full/<int:articl_id>", views.articl_full, name="articl_full"),
+    ]
--- sem01_games_app/views.py
+++ sem01_games_app/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 import random
 import logging
 from sem02_task1_app.models import Drop
+from sem02_task1_app.models import Author
+from sem02_task1_app.models import Article
+from sem02_task1_app.models import Post
+from django.shortcuts import render
 
 
 # Create your views here.
@@ -25,13 +29,49 @@ def game2(request):
     logger.info(f'Сработала игра 2 "кости", выпало {res}')
     return HttpResponse(f"Hello, game2! Выпало значение {res}")
 
-def game3(request):
-    res = random.randint(1,100)
-    logger.info(f'Сработала игра 2 "1213123", выпало {res}')
-    return HttpResponse(f"game3! Выпало значение {res}")
+def game3(request, count_drop: int):
+    game_list = ['ор', 'реш']
+    res = []
+    for i_ in range(count_drop):
+        res.append(random.choice(game_list))
+    exit_cont = {
+        'result_drops': res
+                }
+    return render(request, 'sem01_games_app/start.html', context=exit_cont)
 
 def select_all_n(request, n: int):
     # drops = Drop()
     drops_all = Drop.sum_drops(n)
     # return HttpResponse(f"Было {len(drops_all)} попыток")
     return HttpResponse(drops_all)
+
+def view_start(request, n: str):
+    name = n
+    exit_cont = {
+        'name': name,
+        'prosto_text': 'ssss-----ss'
+            }
+    return render(request, 'sem01_games_app/start.html', context=exit_cont)
+
+def view_author_art(request, author_id: int):
+
+    author = Author.objects.get(pk=author_id)
+    articls = Article.objects.filter(author_id=author_id)
+    # print(author)
+    exit_context = {
+        'author': author.firstname,
+        'articls': articls,
+                }
+    return render(request, 'sem01_games_app/author_articl.html', context=exit_context)
+
+def articl_full(request, articl_id):
+    articl = Article.objects.get(id=articl_id)
+    comments = Post.objects.filter(articl_id=articl_id).order_by('-edit_at')
+    articl.count_view += 1
+    articl.save()
+    context = {
+        'articl':articl,
+        'comments':comments,
+    }
+
+    return render(request, 'sem01_games_app/articl_full.html', context=context)
