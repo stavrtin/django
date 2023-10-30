884d4c49dd7b1302be4eb782aaa5a43541a4b6cd
stavrtin <stavrtin@yandex.ru>
2023-10-30 23:21:11 +0300

    Диплом - рецепты_2

--- prj_recipes/recipes_app/views.py
+++ prj_recipes/recipes_app/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+
 from .models import Recipe, Category, Ingredients
-
-
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .utils import DataMixin
+from .forms import RegisterUserForm
 
 
 def v_start(request):
@@ -24,5 +29,13 @@ def v_page_recepts(request):
     return render(request, "recipes_app/recepts.html", context=context)
 
 
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'recipes_app/register.html'
+    success_url = reverse_lazy('start')
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация", page='register')
 
+        return dict(list(context.items()) + list(c_def.items()))
